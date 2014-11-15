c1ccb81098e4aa9ee6841292e646482660caeaec
Jatin Ganhotra <jatin.ganhotra@gmail.com>
2014-11-15 00:59:22 -0600

    Refactoring and cleanup, Added TODOs - patch.rb

--- patch.rb
+++ patch.rb
@@ -35,27 +35,17 @@ class Patch
     end
 
     def patch_breakdown()
-      #puts "PATCH BREAKDOWN BEGIN"
-      #puts "The original patch is = \n"
-      #puts "Changes start - "
-      #puts @changes
-      #puts "Changes end - "
-
-      #@additions = (@changes).match(/^[\+][^+][^+].*/)
-      #@deletions = (@changes).match(/^[\-][^-][^-].*/)
-      # IMP :- Get the string from the MatchData returned by Regex.match
+      # Get the string from the MatchData returned by Regex.match
       @additions = (@changes).match(/^[\+][^+].*/).to_s
       @deletions = (@changes).match(/^[\-][^-].*/).to_s
-      # puts "Additions are - " + @additions.to_s
-      # puts "Deletions are - " + @deletions.to_s
 
+      # TODO: This still looks buggy. Confirm.
       # Remove the first character +/- from the additions/ deletions string
       @additions[0] = ""
       @deletions[0] = ""
-      #puts "Size of additions = " + @additions.to_s.size.to_s
-      #puts "Size of deletions = " + @deletions.to_s.size.to_s
 
       # FIXME: This would be issued when you have a patch diff like -
+      # Try to see how I handle such cases
       # diff --git a/django-docs/images/flatfiles_admin.png b/django-docs/images/flatfiles_admin.png
       # new file mode 100644
       # index 0000000..391a629
