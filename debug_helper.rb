3f8b453031b266616e80a70c69ff99e811860011
Jatin Ganhotra <jatin.ganhotra@gmail.com>
2014-11-26 00:44:21 -0600

    Updated the PrintDiffInfoAndStats fn. for better debugging

--- debug_helper.rb
+++ debug_helper.rb
@@ -20,18 +20,24 @@ end
 # Input arguments are diff, cmp_diff
 def PrintDiffInfoAndStats(diff,cmp_diff)
   puts "********** PrintDiffInfoAndStats BEGIN ***********"
-  puts "diff is = "
-  ap diff
   puts "diff stats is = \n"
   ap diff.stats
   puts "diff sha's are = "
   puts("\{ #{diff.prev_commit_sha} -> #{diff.next_commit_sha} \}")
+  puts "diff prev commit message is "
+  puts $rugged_repo.lookup(diff.prev_commit_sha).message
+  puts "diff next commit message is "
+  puts $rugged_repo.lookup(diff.next_commit_sha).message
   puts "--------------------------------------------------"
-  puts "cmp_diff is = "
-  ap cmp_diff
+
   puts "cmp_diff stats is = \n"
   ap cmp_diff.stats
   puts "cmp_diff sha's are = "
   puts("\{ #{cmp_diff.prev_commit_sha} -> #{cmp_diff.next_commit_sha} \}")
+  puts "cmp_diff prev commit message is "
+  puts $rugged_repo.lookup(cmp_diff.prev_commit_sha).message
+  puts "cmp_diff next commit message is "
+  puts $rugged_repo.lookup(cmp_diff.next_commit_sha).message
+  puts "--------------------------------------------------"
   puts "********** PrintDiffInfoAndStats END **************"
 end
